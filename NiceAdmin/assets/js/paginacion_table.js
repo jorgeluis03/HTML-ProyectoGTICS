48cdce06cbd94dae3303501efbd25302082c629a
Gustavo <a20192858@pucp.edu.pe>
2023-04-07 17:54:16 -0500

    Paginacion en tablas, pop up, boton loguear, vista superadmin dashboard

---
+++ NiceAdmin/assets/js/paginacion_table.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    function showTableSegment(table, startIndex, endIndex) {
+        table.find('tbody tr').hide()
+            .slice(startIndex, endIndex).show();
+
+        $('#prev-btn').prop('disabled', startIndex === 0);
+        $('#next-btn').prop('disabled', endIndex === table.find('tbody tr').length);
+
+        var currentPage = startIndex / 5 + 1;
+        $('#page-num').text(currentPage);
+    }
+
+    function setupTablePagination(table, itemsPerPage) {
+        var startIndex = 0;
+        var endIndex = itemsPerPage;
+
+        showTableSegment(table, startIndex, endIndex);
+
+        function updateIndex(isNext) {
+            if (isNext) {
+                if (startIndex + itemsPerPage < table.find('tbody tr').length) {
+                    startIndex += itemsPerPage;
+                }
+            } else {
+                if (startIndex > 0) {
+                    startIndex -= itemsPerPage;
+                }
+            }
+            endIndex = startIndex + itemsPerPage;
+            showTableSegment(table, startIndex, endIndex);
+        }
+
+        $('#prev-btn').click(function() {
+            updateIndex(false);
+        });
+
+        $('#next-btn').click(function() {
+            updateIndex(true);
+        });
+    }
+
+    setupTablePagination($('#superadmin-table'), 5);
+
+    var firstName;
+    var lastName;
+    var id1;
+
+    $('#staticBackdrop').on('show.bs.modal', function(event) {
+        var button = event.relatedTarget;
+        var $row = $(button).closest('tr') // Asegúrate de buscar solo filas visibles
+        id1 = $row.find('.row-id').text();
+        firstName = $row.find('td:nth-child(2)').text();
+        lastName = $row.find('td:nth-child(3)').text();
+        console.log(`firstName: ${firstName}, lastName: ${lastName}`);
+        console.log(`ID: ${id1}`);
+        $('#staticBackdropLabel').html(`Iniciar sesión`);
+        $('.modal-body').html(`¿Loguearse como ${firstName} ubicado en la fila ${id1}?` );
+    });
+});
